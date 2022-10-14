7e14b81983230444c2edbf96a60a7f7816f44b49
Gabriel Lemos <jmjgb@outlook.com>
2022-10-14 15:21:04 -0300

    Adicionado barra verde, logotipo Trybewarts, acrescentado formulário de login e titulo Trybewarts

---
+++ script.js
@@ -0,0 +1,13 @@
+const submitButton = document.querySelector('#submit');
+const email = document.querySelector('#input-email');
+const password = document.querySelector('#input-password');
+console.log(submitButton);
+
+submitButton.addEventListener('click', (event) => {
+  event.preventDefault();
+  if (email.value === 'tryber@teste.com' && password.value === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Email ou senha inválidos.');
+  }
+});
